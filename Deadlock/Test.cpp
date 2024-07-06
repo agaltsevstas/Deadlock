649e709c500e16b93ae8bfb23f5c5f056b9550eb
agaltsevstas <agaltsev.stas@mail.ru>
2024-07-06 11:58:13 +0300

    Добавил shell-скрипты

--- Deadlock/Test.cpp
+++ Deadlock/Test.cpp
@@ -1,15 +1,55 @@
 #include "Deadlock.h"
 #include "LockGuard.h"
 #include "Spinlock.h"
-#include "Test.h"
 
+#include <cassert>
 #include <iostream>
 #include <mutex>
 #include <thread>
 
+/*
+ bash: https://github.com/Abd3lwahab/Test-Cases
+ */
 
-int main()
+namespace details
 {
+    template<class T>
+    concept IsDeadlock = requires(const T& deadlock) // Условие: проверка на контейнер
+    {
+        deadlock.Islock();
+        deadlock();
+    };
+}
+
+namespace assert_details
+{
+    template<details::IsDeadlock T>
+    void assert_equals(const T& deadlock)
+    {
+        if (deadlock() == true)
+            std::cerr << "failed!" << std::endl;
+        else
+            std::cerr << "passed!" << std::endl;
+    }
+}
+
+#define ASSERT_DEADLOCK(result) \
+    assert_details::assert_equals(result)
+
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+        throw std::runtime_error("Missing argument!");
+    
+    bool isDeadlock = false;
+    if (strcmp(argv[1], "true") == 0)
+        isDeadlock = true;
+    else if (strcmp(argv[1], "false") == 0)
+        isDeadlock = false;
+    else
+        throw std::runtime_error("Invalid argument!");
+    
     Spinlock mutex1;
     Spinlock mutex2;
     Deadlock deadlock(mutex1, mutex2);
@@ -32,35 +72,19 @@ int main()
     
     /// Deadlock
     {
-        deadlock.SetLock(true); // Включение deadlock
+        deadlock.SetLock(isDeadlock); // Включение deadlock
         std::thread thread1(function1);
         std::thread thread2(function2);
 
         std::this_thread::sleep_for(std::chrono::seconds(1)); // Время на deadlock
         
         ASSERT_DEADLOCK(deadlock); // Проверка на deadlock
-        ASSERT_EQUALS(deadlock() == true, 1); // Проверка на deadlock
         
         deadlock.Destroy(); // Разрушение deadlock
         
         thread1.join();
         thread2.join();
     }
-    /// No Deadlock
-    {
-        deadlock.SetLock(false); // Отключение deadlock
-        std::thread thread1(function1);
-        std::thread thread2(function2);
-
-        std::this_thread::sleep_for(std::chrono::seconds(1)); // Время на deadlock
-        
-        ASSERT_DEADLOCK(deadlock); // Проверка на deadlock
-        ASSERT_EQUALS(deadlock() == false, 1); // Проверка на deadlock
-        
-        thread1.join();
-        thread2.join();
-    }
-
     
     return 0;
 }
---
+++ deadlock-script.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+    
+printf "********* Test-Case Deadlock Script Running *********\n"
+
+# Check command line argument entered correct or not
+file="Deadlock/Test.cpp"
+
+printf "[+] Checking file: "
+if [ -f "$file" ]; then
+    echo "file exist"
+else
+    echo "file does not exist!"
+    exit 1
+fi
+
+exe="Test"
+if [ ! -f "$exe" ]; then
+
+    printf "[+] Start compiling: "
+    make=$(g++ -std=c++20 -o "$exe" "$file")
+    # Check the compiling process success or not to exit
+    if [ -n "$make" ]; then
+        echo "compiling failed!"
+        exit 2
+    else
+        echo "successfully compiled"
+    fi
+fi
+
+printf "[+] Running Deadlock: "
+argument="true"
+run=$(./"$exe" "$argument")
+
+echo "********* Script End succesfully *********"
+exit 0
